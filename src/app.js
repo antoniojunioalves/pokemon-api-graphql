4444a6a59a69fca463b4ed68ae73a7083c26ab55
antoniojunioalves <antoniojunioalves@gmail.com>
2020-12-22 19:52:18 -0300

    Ajustando para adicionar o cors.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
-import Koa from 'koa';
-import cors from 'koa-cors';
-import graphqlHTTP from 'koa-graphql';
-import convert from 'koa-convert';
+import Koa from "koa";
+import cors from "koa-cors";
+import graphqlHTTP from "koa-graphql";
+import convert from "koa-convert";
 
-import schema from './schema';
+import schema from "./schema";
 
 const app = new Koa();
 
-app
-  .use(convert(cors()))
-  .use(convert(graphqlHTTP(async () => ({
-    graphiql: true,
-    schema,
-    formatError: ({ message, locations, stack }) => ({
-      message,
-      locations,
-      stack,
-    }),
-  })
-)));
+app.use(cors());
+app.use(
+  convert(
+    graphqlHTTP(async () => ({
+      graphiql: true,
+      schema,
+      formatError: ({ message, locations, stack }) => ({
+        message,
+        locations,
+        stack,
+      }),
+    }))
+  )
+);
 
 export default app;
